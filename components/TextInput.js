6b8f54952f797a583b8732feb5293a14c6d01527
myles <myles.sanigar@gmail.com>
2016-09-21 21:53:41 +0100

    MS : react - update parent component and state from child

--- components/TextInput.js
+++ components/TextInput.js
@@ -16,6 +16,12 @@ class TextInput extends Component {
 		})
 	}
 
+	deleteLetter() {
+		this.setState({
+			inputText: this.state.inputText.substring(0, this.state.inputText.length - 1)
+		})
+	}
+
 	render() {
 		return (
 			<div className="input">
@@ -28,7 +34,10 @@ class TextInput extends Component {
 				value={this.state.inputText}
 				onChange={this.handleChange.bind(this)}
 				/>
-				<TextDisplay text={this.state.inputText}/>
+				<TextDisplay 
+				text={this.state.inputText} 
+				deleteLetter={this.deleteLetter.bind(this)}
+				/>
 			</div>
 		)
 	}
